8240bf8cc6884df14b9b0369d94e31e5d58abac5
Usman Ali <160613385+UsmanAliNow@users.noreply.github.com>
2024-03-18 04:00:16 +0500

    Add files via upload

---
+++ 13. 1st Coversion.py
@@ -0,0 +1,6 @@
+
+#1. Conversion:
+a = 2           # Int value
+b = 4.25        # Float value
+
+print(a+b)      #Answer is 6.25 float value because Float value is superior
---
+++ 14. 2nd Casting.py
@@ -0,0 +1,14 @@
+
+#2. Casting:
+a = int("2")           # This value was String and covert into Integers value
+b = 4.25               # Float value
+
+print(a+b)             #Answer is 6.25 float value because Float value is superior
+
+
+#Float Value covert into string
+a = 2.14
+a = str(a)
+
+print(type(a))
+
